66afa85d5168a48bae831db5aa8bc6a6ee81b96a
sujkim <13511934+sujkim@users.noreply.github.com>
2021-05-04 22:14:00 -0400

    Update program6_kimsue.asm

--- program6_kimsue.asm
+++ program6_kimsue.asm
@@ -2,9 +2,6 @@ TITLE Designing LowLevel IO Procedures   (program6_kimsue.asm)
 
 ; Author: Sue Kim
 ; Last Modified: 9 Jun 2020
-; OSU email address: kimsue@oregonstate.edu
-; Course number/section: CS271-400C
-; Project Number: Program 6               Due Date:7 Jun 2020
 ; Description: This program gets 10 signed integers from the user and stores the values in an array. The program
 ;				then displays the integers, their sum, and their average
 
@@ -27,13 +24,13 @@ getString	MACRO	prompt, input
 ;-----------------------------------------------------------------------------
 	push	ecx
 	push	edx
-	mov		edx, prompt
+	mov	edx, prompt
 	call	WriteString			; display prompt
-	mov		edx, input			 
-	mov		ecx, 12
+	mov	edx, input			 
+	mov	ecx, 12
 	call	Readstring			; store input into memory location input
-	pop		edx
-	pop		ecx
+	pop	edx
+	pop	ecx
 
 ENDM
 
@@ -44,21 +41,23 @@ displayString	MACRO   memory_location
 ; receives:
 ;	memory_location = OFFSET of where the string is stored
 ; postconditions: string is displayed
+;-------------------------------------------------------------------------------
+
 	push	edx
-	mov		edx, memory_location
+	mov	edx, memory_location
 	call	WriteString
-	pop		edx
+	pop	edx
 
 ENDM
 ;--------------------------------------------------------------------------------------
 
 .data
 
-intro_1				BYTE		"Designing Low-Level I/O procedures			By: Sue Kim", 0
-intro_2				BYTE		"Please provide 10 signed decimal integers.", 0dh, 0ah
-					BYTE		"Each number needs to be small enough to fit inside a 32 bit register.", 0dh, 0ah
-					BYTE		"After you have finished inputting the raw numbers I will display a list", 0dh, 0ah
-					BYTE		"of the integers, their sum, and their average value", 0dh, 0ah, 0
+intro_1			BYTE		"Designing Low-Level I/O procedures			By: Sue Kim", 0
+intro_2			BYTE		"Please provide 10 signed decimal integers.", 0dh, 0ah
+			BYTE		"Each number needs to be small enough to fit inside a 32 bit register.", 0dh, 0ah
+			BYTE		"After you have finished inputting the raw numbers I will display a list", 0dh, 0ah
+			BYTE		"of the integers, their sum, and their average value", 0dh, 0ah, 0
 integer_prompt		BYTE		"Please enter a signed number: ", 0
 integer_input		BYTE		30 DUP(0)	
 error_message		BYTE		"You did not enter a signed number or your number was too big.", 0
@@ -66,13 +65,13 @@ tryAgain_prompt		BYTE		"Please try again: ", 0
 numbers_display		BYTE		"You entered the following numbers: ", 0
 fill_counter		DWORD		?
 list_numbers		SDWORD		ARRAYSIZE	DUP(?)		; list of 10 signed integers
-comma_space			BYTE		", ", 0
-output				BYTE		12  DUP(0)		
+comma_space		BYTE		", ", 0
+output			BYTE		12  DUP(0)		
 reversed_output		BYTE		12  DUP(0)
-sum					SDWORD		?				; calculated sum
-sum_display			BYTE		"The sum of these numbers is: ", 0
+sum			SDWORD		?				; calculated sum
+sum_display		BYTE		"The sum of these numbers is: ", 0
 average_display		BYTE		"The rounded average is: ", 0
-goodbye				BYTE		"Thanks for playing!", 0
+goodbye			BYTE		"Thanks for playing!", 0
 
 	
 .code
@@ -140,20 +139,20 @@ introduction		PROC
 ; registers changed: edx
 ;---------------------------------------------------------------------------
 	push	ebp
-	mov		ebp, esp
-	mov		edx, [ebp+12]		
+	mov	ebp, esp
+	mov	edx, [ebp+12]		
 	call	WriteString			; display program name and programmer
 	call	CrLf
 	call	CrLf
-	mov		edx, [ebp+8]
+	mov	edx, [ebp+8]
 	call	WriteString			; display functionality of program
 	call	CrLf
 
 	; restore ebp
-	pop		ebp
+	pop	ebp
 	
-	ret		8
-introduction		ENDP
+	ret	8
+introduction	ENDP
 
 
 ;-----------------------------------------------------------------------------
@@ -173,19 +172,19 @@ ReadVal				PROC
 ;-------------------------------------------------------------------------------------
 
 	push		ebp
-	mov			ebp, esp
-	mov			edi, [ebp+8]		; @ list_numbers
-	mov			ecx, [ebp+32]		; list size 
+	mov		ebp, esp
+	mov		edi, [ebp+8]		; @ list_numbers
+	mov		ecx, [ebp+32]		; list size 
 			
 getInput:	
 	getString	[ebp+16], [ebp+12]	; prompts for digits, gets string of digits from user
-	mov			[ebp+28], ecx		; save list counter
+	mov		[ebp+28], ecx		; save list counter
 
 ; validate user's input to make sure it is a signed integer
 validate:
-	mov			ecx, eax			; set counter to number of characters
+	mov			ecx, eax		; set counter to number of characters
 	mov			esi, [ebp+12]		; @ user's string input to esi
-	cld								; set to forward direction
+	cld						; set to forward direction
 	lodsb							
 
 ; check if first character is a sign (+ or -)	
@@ -197,7 +196,7 @@ validate:
 	
 ; check if character is a digit
 checkDigit:
-	push		ecx
+	push			ecx
 	cmp			al, LO_STRING		; str[k] < 48
 	jl			invalid
 	cmp			al, HI_STRING		; str[k] > 57
@@ -205,32 +204,32 @@ checkDigit:
 
 ; convert string digit to integer 
 ; equation: x = 10*x + (str[k] - 48) where x = integer
-	sub			al, LO_STRING		; subtract 48 from character
-	movzx		ecx, al				; result (str[k]-48) in ecx
+	sub			al, LO_STRING			; subtract 48 from character
+	movzx			ecx, al				; result (str[k]-48) in ecx
 	mov			eax, edx			; current value of x
 	mov			ebx, 10				
-	imul		ebx					; multiply  x by 10 
+	imul			ebx				; multiply  x by 10 
 	jo			invalid				; check overflow
 	add			eax, ecx			; 10*x + (str[k] - 48)
 	jo			invalid				; check overflow
 	mov			edx, eax			; move result to edx
-	pop			ecx					; restore counter for number of characters
+	pop			ecx				; restore counter for number of characters
 	lodsb							; get next character
-	loop		checkDigit			; validate rest of characters
+	loop			checkDigit			; validate rest of characters
 
 ; if string is negative, negate final result
-	mov			esi, [ebp+12]		; start from beginning
+	mov			esi, [ebp+12]			; start from beginning
 	lodsb
 	cmp			al, MINUS			; check if first character is a - 
 	jne			write
-	neg			edx					; negate x if negative string digit
+	neg			edx				; negate x if negative string digit
 
 ; write valid input to list of numbers
 write:
-	mov			ecx, [ebp+28]		; restore list counter
+	mov			ecx, [ebp+28]			; restore list counter
 	mov			[edi], edx			; add result to list_numbers
 	add			edi, 4				; move to next element
-	loop		getInput			; get next number
+	loop			getInput			; get next number
 	jmp			done
 	
 ; if first character is a sign, move to next character to validate if its a digit
@@ -244,9 +243,9 @@ sign:
 invalid:
 	pop			ecx
 	mov			edx, [ebp+20]
-	call		WriteString			; display error message
-	call		CrLf
-	getString	[ebp+24], [ebp+12]	; displays prompt to try again, gets user input
+	call			WriteString			; display error message
+	call			CrLf
+	getString		[ebp+24], [ebp+12]		; displays prompt to try again, gets user input
 	jmp			validate
 
 done:
@@ -269,23 +268,23 @@ displayList		PROC
 ; registers changed: eax, ecx, esi
 ;----------------------------------------------------------
 	push			ebp
-	mov				ebp, esp
-	mov				esi, [ebp+16]			; @ list_numbers in esi
-	mov				ecx, [ebp+8]			; set loop counter to list size
+	mov			ebp, esp
+	mov			esi, [ebp+16]			; @ list_numbers in esi
+	mov			ecx, [ebp+8]			; set loop counter to list size
 	
-	displayString	[ebp+12]				; displays title of list
+	displayString		[ebp+12]			; displays title of list
 	call			CrLf
 	
 ; displays the list of integer values as string digits
 display:
-	mov				eax, [esi]				; value in integer list 
-	push			[ebp+20]				; string output 
-	push			[ebp+24]				; temp reversed string
-	call			WriteVal				; display number as string
-	cmp				ecx, 1
-	je				done
-	displayString	[ebp+28]				; add comma if unfinished
-	add				esi, 4					; next element	
+	mov			eax, [esi]			; value in integer list 
+	push			[ebp+20]			; string output 
+	push			[ebp+24]			; temp reversed string
+	call			WriteVal			; display number as string
+	cmp			ecx, 1
+	je			done
+	displayString		[ebp+28]			; add comma if unfinished
+	add			esi, 4				; next element	
 	loop			display
 
 done:
@@ -312,26 +311,26 @@ displaySum		PROC
 ;----------------------------------------------------
 	
 	push			ebp
-	mov				ebp, esp
-	mov				esi, [ebp+8]			; @ list_numbers 
-	mov				ecx, [ebp+20]			; list size
-	mov				eax, 0					; initialize accumulator to 0
+	mov			ebp, esp
+	mov			esi, [ebp+8]			; @ list_numbers 
+	mov			ecx, [ebp+20]			; list size
+	mov			eax, 0				; initialize accumulator to 0
 
 ; calculates the sum of numbers by adding first number to next until end of list
 ;	is reached
 calculate:
-	mov				ebx, [esi]				; number at list position
-	add				eax, ebx				; add to accumulator
-	add				esi, 4					; move to next element
-	loop			calculate				; repeat until end of list
-	mov				ebx, [ebp+16]			; @ sum variable to ebx
-	mov				[ebx], eax				; save sum to sum variable
+	mov			ebx, [esi]			; number at list position
+	add			eax, ebx			; add to accumulator
+	add			esi, 4				; move to next element
+	loop			calculate			; repeat until end of list
+	mov			ebx, [ebp+16]			; @ sum variable to ebx
+	mov			[ebx], eax			; save sum to sum variable
 
 	displayString	[ebp+12]				; display title of sum
 	
-	push			[ebp+28]				; @ output
-	push			[ebp+24]				; @ temp reversed string
-	call			WriteVal				; calculated sum in eax register
+	push			[ebp+28]			; @ output
+	push			[ebp+24]			; @ temp reversed string
+	call			WriteVal			; calculated sum in eax register
 	
 	; restore ebp
 	pop				ebp
@@ -353,17 +352,17 @@ displayAverage		PROC
 ; registers changed: eax, ebx, edx
 ;-------------------------------------------------------------------------------
 	push			ebp
-	mov				ebp, esp
-	mov				ebx, [ebp+12]		; @ sum of list 
-	mov				eax, [ebx]
-	mov				ebx, [ebp+16]		; size of list
+	mov			ebp, esp
+	mov			ebx, [ebp+12]		; @ sum of list 
+	mov			eax, [ebx]
+	mov			ebx, [ebp+16]		; size of list
 	cdq	
-	idiv			ebx					; divide sum by number of integers (list size)
+	idiv			ebx			; divide sum by number of integers (list size)
 	
-	displayString	[ebp+8]				; display "average of numbers is: "
+	displayString		[ebp+8]			; display "average of numbers is: "
 
-	push			[ebp+24]			; string output
-	push			[ebp+20]			; reversed temp string
+	push			[ebp+24]		; string output
+	push			[ebp+20]		; reversed temp string
 	call			WriteVal			
 
 	; restore ebp
@@ -387,82 +386,82 @@ WriteVal			PROC
 	push			ecx
 	push			esi
 
-	mov				edi, [ebp+8]		; @ temporary reversed string
-	mov				ecx, eax
-	mov				ebx, 0				; initialize counter for number of characters
-	push			eax					; save original signed value
-	cmp				eax, 0		
-	jge				convert				; skip if number is positive
-	neg				ecx					; change number to positive 
-	cld									; set to forwards direction
+	mov			edi, [ebp+8]		; @ temporary reversed string
+	mov			ecx, eax
+	mov			ebx, 0			; initialize counter for number of characters
+	push			eax			; save original signed value
+	cmp			eax, 0		
+	jge			convert			; skip if number is positive
+	neg			ecx			; change number to positive 
+	cld						; set to forwards direction
 
 ; converts an integer to string by converting each string starting from 1s 
 ;	then 10s, then 100s, ... adds 48 to digit to convert to ASCII character
 ;	string is stored in reverse order
 convert:
-	push			ebx					; save counter for number of characters
-	mov				eax, ecx			
-	mov				ebx, 10	
+	push			ebx			; save counter for number of characters
+	mov			eax, ecx			
+	mov			ebx, 10	
 	cdq
-	idiv			ebx					; divide number by 10	
-	mov				ecx, eax			; save quotient
-	mov				al, dl				; move remainder to al
-	add				al, LO_STRING		; add 48 to digit to convert to ASCII
+	idiv			ebx			; divide number by 10	
+	mov			ecx, eax		; save quotient
+	mov			al, dl			; move remainder to al
+	add			al, LO_STRING		; add 48 to digit to convert to ASCII
 	stosb
-	pop				ebx					; restore character counter
-	inc				ebx					
-	cmp				ecx, 10				; if there's more digits, repeat
-	jge				convert				
-	cmp				ecx, 0				
-	je				sign				; if quotient is 0, no more digits, check if its a sign
-	mov				al, cl				
-	add				al, LO_STRING		; convert quotient
+	pop			ebx			; restore character counter
+	inc			ebx					
+	cmp			ecx, 10			; if there's more digits, repeat
+	jge			convert				
+	cmp			ecx, 0				
+	je			sign			; if quotient is 0, no more digits, check if its a sign
+	mov			al, cl				
+	add			al, LO_STRING		; convert quotient
 	stosb			
-	inc				ebx
+	inc			ebx
 ; checks if entered number is negative, if so, adds a minus character 
 sign:
-	pop				eax					; restore original number
-	cmp				eax, 0								
-	jge				continue			; if positive, skip
-	mov				al, MINUS			; if negative, add - 
+	pop			eax			; restore original number
+	cmp			eax, 0								
+	jge			continue		; if positive, skip
+	mov			al, MINUS		; if negative, add - 
 	stosb	
-	inc				ebx
+	inc			ebx
 	
 continue:
 ; writes the temporary reversed string to output string in reversed order	
-	mov				esi, [ebp+12]		; @ output list in esi
-	mov				ecx, ebx			; set counter
-	dec				edi				
+	mov			esi, [ebp+12]		; @ output list in esi
+	mov			ecx, ebx		; set counter
+	dec			edi				
 reverse:
-	mov				al, [edi]			; move element to eax
-	mov				[esi], al			
-	dec				edi					; move backwards from reversed string
-	inc				esi					; move to next element in output
+	mov			al, [edi]		; move element to eax
+	mov			[esi], al			
+	dec			edi			; move backwards from reversed string
+	inc			esi			; move to next element in output
 	loop			reverse			
 ; displays contents of the output string	
 done:	
-	mov				esi, [ebp+12]		; move to beginning of list
-	displayString	esi 				; display final output
-	mov				ecx, ebx			; set character length as loop counter
-	mov				edi, [ebp+8]		; move to beginning of list
+	mov			esi, [ebp+12]		; move to beginning of list
+	displayString		esi 			; display final output
+	mov			ecx, ebx		; set character length as loop counter
+	mov			edi, [ebp+8]		; move to beginning of list
 
 ; clear the temp reversed string and output string
 clearArrays:
-	mov				al, 0
-	mov				[esi], al
-	mov				[edi], al
-	inc				esi
-	inc				edi
+	mov			al, 0
+	mov			[esi], al
+	mov			[edi], al
+	inc			esi
+	inc			edi
 	loop			clearArrays
 
 	; restore registers from calling function
-	pop				esi
-	pop				ecx
+	pop			esi
+	pop			ecx
 
 	; restore ebp
-	pop				ebp
+	pop			ebp
 	
-	ret				8		
+	ret			8		
 WriteVal			ENDP
 
 
@@ -474,8 +473,8 @@ farewell			PROC
 ; registers changed: edx
 ;--------------------------------------
 	push			ebp
-	mov				ebp, esp
-	displayString	[ebp+8]			; display farewell message
+	mov			ebp, esp
+	displayString		[ebp+8]			; display farewell message
 	call	CrLf
 
 	; restore ebp
